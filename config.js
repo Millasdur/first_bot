aa6dd9aaa5d8df65aaf22a1b7d8140a3bf467067
Hugo LELY <hlely@student.42.fr>
2018-05-07 08:20:34 +0200

    added movie

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     PORT: 5000,
     WEATHER_TOKEN : 'a357ff0d3819eb091d827cfacd7382b6',
+    MOVIE_TOKEN : 'b85331bcd15aa58411d69643dc13ca60'
   };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./config.js');
 const getWeather = require('./bot.js');
+const movie = require('./movie.js')
 
 const app = express();
 app.use(bodyParser.json());
@@ -29,5 +30,33 @@ app.post('/bot', (req, res) => {
     location = null;
     date = null;
 })
+
+app.post('/movie', (req, res) => {
+    console.log('[POST] /movie');
+    const memory = req.body.conversation.memory;
+    const movie = memory.movie;
+    const tv = memory.tv;
+
+    // Check for the presence of entities movie or tv
+    // If both are present, we prioritize movie
+    const kind = movie ? 'movie' : 'tv';
+
+    const genre = memory.genre;
+    const genreId = constants.movie.getGenreId(genre.value);
+
+    const language = memory.language;
+    const nationality = memory.nationality;
+
+    // Similar to movie and tv, we prioritize language over nationality
+    const isoCode = language
+      ? language.short.toLowerCase()
+      : nationality.short.toLowerCase();
+
+    return movie.discoverMovie(kind, genreId, isoCode)
+      .then((carouselle) => res.json({
+        replies: carouselle,
+      }))
+      .catch((err) => console.error('movieApi::discoverMovie error: ', err));
+  });
 
 app.listen(process.env.PORT || 5000, () => console.log(`App started on port ${config.PORT}`));
