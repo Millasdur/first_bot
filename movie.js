c8b0c0beb22b60c347f19cd0747de95b0028fab2
Hugo LELY <hlely@student.42.fr>
2018-05-07 08:42:08 +0200

    test

--- movie.js
+++ movie.js
@@ -27,62 +27,62 @@ const movieGenres = [
     { id: 10752, name: 'War' },
     { id: 10770, name: 'TV Movie' },
   ];
-  
-  // Find the moviedb id of a genre entity
-  function getGenreId(genre) {
-    const row = movieGenres.find(function(elem) {
-      return elem.name.toLowerCase() === genre.toLowerCase();
-    });
-  
-    if (row) {
-      return row.id;
-    }
-    return null;
-  }
 
-  function discoverMovie(kind, genreId, language) {
-    return axios.get(`https://api.themoviedb.org/3/discover/${kind}`, {
-      params: {
-        api_key: config.MOVIE_TOKEN,
-        sort_by: 'popularity.desc',
-        include_adult: false,
-        with_genres: genreId,
-        with_original_language: language,
-      },
-    }).then(results => {
-      if (results.length === 0) {
-        return [{
-          type: 'quickReplies',
-          content: {
-            title: 'Sorry, but I could not find any results for your request :(',
-            buttons: [{ title: 'Start over', value: 'Start over' }],
-          },
-        }];
-      }
-  
-      const cards = results.slice(0, 10).map(movie => ({
-        title: movie.title || movie.name,
-        subtitle: movie.overview,
-        imageUrl: `https://image.tmdb.org/t/p/w640${movie.poster_path}`,
-        buttons: [
-          {
-            type: 'web_url',
-            value: `https://www.themoviedb.org/movie/${movie.id}`,
-            title: 'View More',
-          },
-        ],
-      }));
-  
-      return [
-        {
-          type: 'text',
-          content: "Here's what I found for you!",
+// Find the moviedb id of a genre entity
+function getGenreId(genre) {
+const row = movieGenres.find(function(elem) {
+    return elem.name.toLowerCase() === genre.toLowerCase();
+});
+
+if (row) {
+    return row.id;
+}
+return null;
+}
+
+function discoverMovie(kind, genreId, language) {
+return axios.get(`https://api.themoviedb.org/3/discover/${kind}`, {
+    params: {
+    api_key: config.MOVIE_TOKEN,
+    sort_by: 'popularity.desc',
+    include_adult: false,
+    with_genres: genreId,
+    with_original_language: language,
+    },
+}).then(results => {
+    if (results.length === 0) {
+    return [{
+        type: 'quickReplies',
+        content: {
+        title: 'Sorry, but I could not find any results for your request :(',
+        buttons: [{ title: 'Start over', value: 'Start over' }],
         },
-        { type: 'carousel', content: cards },
-      ];
-    });
-  }
-  
-  module.exports.discoverMovie() = discoverMovie;
+    }];
+    }
 
-  module.exports.getGenreId() = getGenreId;
+    const cards = results.slice(0, 10).map(movie => ({
+    title: movie.title || movie.name,
+    subtitle: movie.overview,
+    imageUrl: `https://image.tmdb.org/t/p/w640${movie.poster_path}`,
+    buttons: [
+        {
+        type: 'web_url',
+        value: `https://www.themoviedb.org/movie/${movie.id}`,
+        title: 'View More',
+        },
+    ],
+    }));
+
+    return [
+    {
+        type: 'text',
+        content: "Here's what I found for you!",
+    },
+    { type: 'carousel', content: cards },
+    ];
+});
+}
+
+module.exports.discoverMovie = discoverMovie;
+
+module.exports.getGenreId = getGenreId;
