00620124bc1545f9a07934e4343bc5850af883fc
Hugo LELY <hlely@student.42.fr>
2018-05-07 08:46:38 +0200

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./config.js');
 const getWeather = require('./bot.js');
-const movie = require('./movie.js')
+const discoverMovie = require('./movie.js')
 
 const app = express();
 app.use(bodyParser.json());
